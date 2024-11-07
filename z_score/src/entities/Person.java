fd945ff8e3315f49592eacad85a9f3ecebb79d22
Ryann <ryann.arruda_159@hotmail.com>
2024-11-07 16:44:14 -0300

    Entity Person

---
+++ z_score/src/entities/Person.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public abstract class Person {
+	private String name;
+	private Date date_birth;
+	
+	public Person(String name, Date date_birth) {
+		this.name = name;
+		this.date_birth = date_birth;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate_birth() {
+		return date_birth;
+	}
+
+	public void setDate_birth(Date date_birth) {
+		this.date_birth = date_birth;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date_birth, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(date_birth, other.date_birth) && Objects.equals(name, other.name);
+	}	
+}
