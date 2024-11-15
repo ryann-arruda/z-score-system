ce4b95b6b91f32c56534e51dab4d40d889e72655
Ryann <ryann.arruda_159@hotmail.com>
2024-11-15 17:42:14 -0300

    Implementation of the findByd method

--- z_score/src/repository/impl/MeasurementZscoreDaoImpl.java
+++ z_score/src/repository/impl/MeasurementZscoreDaoImpl.java
@@ -1,8 +1,13 @@
 package repository.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
+import db.DBException;
 import entities.MeasurementZscore;
 import repository.MeasurementZscoreDao;
 
@@ -11,6 +16,16 @@ public class MeasurementZscoreDaoImpl implements MeasurementZscoreDao{
 	
 	public MeasurementZscoreDaoImpl(Connection conn) {
 		this.conn = conn;
+	}
+	
+	private MeasurementZscore instantiateMeasurementZscore(ResultSet rs) throws SQLException {
+		MeasurementZscore msz = new MeasurementZscore();
+		
+		msz.setId(rs.getLong("measurement_zscore_id"));
+		msz.setzScore(rs.getDouble("zscore"));
+		msz.setDate(new Date(rs.getDate("zscore_date").getTime()));
+		
+		return msz;
 	}
 
 	@Override
@@ -33,8 +48,26 @@ public class MeasurementZscoreDaoImpl implements MeasurementZscoreDao{
 
 	@Override
 	public MeasurementZscore findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		MeasurementZscore mzs = null;
+		
+		try {
+			ps = conn.prepareStatement("SELECT * FROM MeasurementZscore WHERE measurementzscore = ?");
+			
+			ps.setLong(1, 1);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				mzs = instantiateMeasurementZscore(rs);
+			}
+		}
+		catch(SQLException e) {
+			throw new DBException("ID was null");
+		}
+		
+		return mzs;
 	}
 
 	@Override
