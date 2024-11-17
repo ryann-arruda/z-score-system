364847d6b7574c7ee7acd29bdff58473ec10a042
Ryann <ryann.arruda_159@hotmail.com>
2024-11-17 02:37:35 -0300

    Implementation of the insert and deleteById methods, changing the type from Integer to Long in the signatures and fixing a bug in the findById method

--- z_score/src/repository/impl/MeasurementZscoreDaoImpl.java
+++ z_score/src/repository/impl/MeasurementZscoreDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.List;
 
@@ -29,9 +30,38 @@ public class MeasurementZscoreDaoImpl implements MeasurementZscoreDao{
 	}
 
 	@Override
-	public boolean insert(MeasurementZscore obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public Long insert(MeasurementZscore obj) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int rowsAffected = -1;
+		Long measurementZscoreId = -1L;
+		
+		try {
+			if(obj.getId() == null) {
+				ps = conn.prepareStatement("INSERT INTO MeasurementZscore(zscore, zscore_date) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+				
+				ps.setDouble(1, obj.getzScore());
+				ps.setDate(2, new java.sql.Date(obj.getDate().getTime()));
+				
+				rowsAffected = ps.executeUpdate();
+				
+				if(rowsAffected > 0) {
+					rs = ps.getGeneratedKeys();
+					
+					while(rs.next()) {
+						measurementZscoreId = rs.getLong(1);
+					}
+				}
+			}
+		}
+		catch(SQLException e) {
+			throw new DBException("Unable to insert a new z-score measurement into the database");
+		}
+		catch(NullPointerException e) {
+			throw new DBException("Cannot insert a null object");
+		}
+		
+		return measurementZscoreId;
 	}
 
 	@Override
@@ -41,21 +71,42 @@ public class MeasurementZscoreDaoImpl implements MeasurementZscoreDao{
 	}
 
 	@Override
-	public boolean deleteById(Integer id) {
-		// TODO Auto-generated method stub
+	public boolean deleteById(Long id) {
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
+		
+		try {
+			MeasurementZscore msz = findById(id);
+			
+			if(msz != null) {
+				ps = conn.prepareStatement("DELETE FROM MeasurementZscore WHERE measurement_zscore_id = ?");
+				
+				ps.setLong(1, id);
+				
+				rowsAffected = ps.executeUpdate();
+				
+				if(rowsAffected > 0) {
+					return true;
+				}
+			}
+		}
+		catch(SQLException e) {
+			throw new DBException("Unable to remove this z-score measurement from the database");
+		}
+		
 		return false;
 	}
 
 	@Override
-	public MeasurementZscore findById(Integer id) {
+	public MeasurementZscore findById(Long id) {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		MeasurementZscore mzs = null;
 		
 		try {
-			ps = conn.prepareStatement("SELECT * FROM MeasurementZscore WHERE measurementzscore = ?");
+			ps = conn.prepareStatement("SELECT * FROM MeasurementZscore WHERE measurement_zscore_id = ?");
 			
-			ps.setLong(1, 1);
+			ps.setLong(1, id);
 			
 			rs = ps.executeQuery();
 			
