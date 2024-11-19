9fbffa89ccfb761ad568cc2f3f216d83acbd9616
Ryann <ryann.arruda_159@hotmail.com>
2024-11-19 12:44:59 -0300

    Adding id field and access and modification methods

--- z_score/src/entities/Child.java
+++ z_score/src/entities/Child.java
@@ -5,12 +5,22 @@ import java.util.Date;
 import java.util.List;
 
 public class Child extends Person{
+	private Long id;
 	private List<MeasurementZscore> zScores;
 
 	public Child(String name, Date date_birth) {
 		super(name, date_birth);
 		
+		id = null;
 		zScores = new ArrayList<>();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 	
 	public boolean addZscore(MeasurementZscore mzs) {
