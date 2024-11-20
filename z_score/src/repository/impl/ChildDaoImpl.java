987e04f99a611ce9c153349a3d27f23d0a3b056e
Ryann <ryann.arruda_159@hotmail.com>
2024-11-20 18:08:01 -0300

    Finalizing the insertion method

--- z_score/src/repository/impl/ChildDaoImpl.java
+++ z_score/src/repository/impl/ChildDaoImpl.java
@@ -5,12 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import db.DBException;
 import db.Database;
 import entities.Child;
+import entities.MeasurementZscore;
 import repository.ChildDao;
+import repository.DaoFactory;
+import repository.MeasurementZscoreDao;
 
 public class ChildDaoImpl implements ChildDao{
 	private Connection conn;
@@ -21,20 +26,26 @@ public class ChildDaoImpl implements ChildDao{
 
 	@Override
 	public Long insert(Child obj) {
+		MeasurementZscoreDao mszImpl = DaoFactory.createMeasurementZscoreDao();
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		int rowsAffected = -1;
 		Long childId = null;
 		
 		try {
-			if(obj.getId() != null) {
+			if(obj.getId() == null) {
 				ps = conn.prepareStatement("INSERT INTO Child(child_name, date_birth) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
 				conn.setAutoCommit(false);
 				
 				ps.setString(1, obj.getName());
 				ps.setDate(2, new java.sql.Date(obj.getDate_birth().getTime()));
 				
-				// TODO Enter all z-score measurements
+				List<Long> measurementZscoreIds = new ArrayList<>();
+				for(MeasurementZscore msz : obj.getAllZscores()) {
+					Long id = mszImpl.insert(msz);
+					msz.setId(id);
+					measurementZscoreIds.add(id);
+				}
 				
 				rowsAffected = ps.executeUpdate();
 				
@@ -43,7 +54,10 @@ public class ChildDaoImpl implements ChildDao{
 					
 					while(rs.next()) {
 						childId = rs.getLong(1);
-						conn.setAutoCommit(true);
+						
+						insertRelationships(measurementZscoreIds, childId);
+												
+						conn.commit();
 					}
 				}
 			}
@@ -51,9 +65,8 @@ public class ChildDaoImpl implements ChildDao{
 		catch(SQLException e) {
 			try {
 				conn.rollback();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+			} catch (SQLException e2) {
+				throw new DBException("It was not possible to insert a new Child object and undo the one that was already inserted");
 			}
 			throw new DBException("Unable to insert a new Child object into the database");
 		}
@@ -66,6 +79,32 @@ public class ChildDaoImpl implements ChildDao{
 		}
 		
 		return childId;
+	}
+
+	private void insertRelationships(List<Long> measurementZscoreIds, Long childId) {
+		PreparedStatement ps = null;
+		
+		try {
+			for(Long id : measurementZscoreIds) {
+				int rowsAffected = -1;
+				ps = conn.prepareStatement("INSERT INTO Child_MeasurementZscore(child_id, measurement_zscore_id) VALUES(?,?)");
+				
+				ps.setLong(1, childId);
+				ps.setLong(2, id);
+				
+				rowsAffected = ps.executeUpdate();
+				
+				if(rowsAffected < 0) {
+					throw new DBException("Unable to insert relationship between Child object and MeasurementZscore object");
+				}
+			}
+		}
+		catch(SQLException e) {
+			throw new DBException("Unable to insert relationship between Child object and MeasurementZscore object");
+		}
+		finally {
+			Database.closeStatement(ps);
+		}
 	}
 
 	@Override
