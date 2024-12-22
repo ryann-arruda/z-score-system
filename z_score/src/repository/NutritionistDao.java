1dbdde08004cfc42570115f668de73e8d6b60e49
Ryann <ryann.arruda_159@hotmail.com>
2024-12-22 11:35:08 -0300

    Changing types from Integer to Long and changing the return type of the insert method to Long

--- z_score/src/repository/NutritionistDao.java
+++ z_score/src/repository/NutritionistDao.java
@@ -1,13 +1,10 @@
 package repository;
 
-import java.util.List;
-
 import entities.Nutritionist;
 
 public interface NutritionistDao {
-	boolean insert(Nutritionist obj);
+	Long insert(Nutritionist obj);
 	boolean update(Nutritionist obj);
-	boolean deleteById(Integer id);
-	Nutritionist findById(Integer id);
-	List<Nutritionist> findAll();
+	boolean deleteById(Long id);
+	Nutritionist findById(Long id);
 }
