2336433d75004bb05e5ca75971bc3b18cd150bdf
Ryann <ryann.arruda_159@hotmail.com>
2025-01-02 16:52:09 -0300

    Implementation of the dialog form window

--- z_score/src/gui/controller/AuthenticationController.java
+++ z_score/src/gui/controller/AuthenticationController.java
@@ -1,17 +1,25 @@
 package gui.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import entities.Nutritionist;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import util.Utils;
 
 public class AuthenticationController implements Initializable{
 	@FXML
@@ -32,6 +40,35 @@ public class AuthenticationController implements Initializable{
 	@FXML
 	public void onLogin() {
 		System.out.println("Ok");
+	}
+	
+	@FXML
+	public void onRegister(ActionEvent event) {
+		Stage parentStage = Utils.getCurrentStage(event);
+		Nutritionist nutritionist = new Nutritionist();
+		
+		createDialogForm(nutritionist, "../../gui/Register_view.fxml", parentStage);
+	}
+
+	private void createDialogForm(Nutritionist nutritionist, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			AnchorPane anchorPane = loader.load();
+			
+			RegisterController registerController = loader.getController();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Cadastro de Nutricionista");
+			dialogStage.setScene(new Scene(anchorPane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.show();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
