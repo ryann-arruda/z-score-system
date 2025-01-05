930dc11263a4456d79d91284a505d8eab973aedf
Ryann <ryann.arruda_159@hotmail.com>
2025-01-05 10:06:43 -0300

    Adding new graphic fields

--- z_score/src/gui/controller/RegisterController.java
+++ z_score/src/gui/controller/RegisterController.java
@@ -3,6 +3,8 @@ package gui.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterController {
@@ -14,6 +16,18 @@ public class RegisterController {
 	
 	@FXML
 	private TextField regionalCouncilNutritionists;
+	
+	@FXML
+	private TextField username;
+	
+	@FXML
+	private PasswordField password;
+	
+	@FXML
+	private PasswordField passwordConfirmation;
+	
+	@FXML
+	private Label errorDisplay;
 	
 	@FXML
 	private Button register;
