7013097ed41fce6e7f2edf7f8f1ec72615354e94
Ryann <ryann.arruda_159@hotmail.com>
2025-01-07 16:34:12 -0300

    Implementing ValidateException

---
+++ z_score/src/exceptions/ValidateException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> errors = new HashMap<>();
+	
+	public ValidateException(String msg) {
+		super(msg);
+	}
+	
+	public Map<String, String> getErros() {
+		return new HashMap<>(errors);
+	}
+	
+	public void addError(String fieldName, String errorMessage) {
+		errors.put(fieldName, errorMessage);
+	}
+}
