1421df26847abfa22dfec21f463b6101e035849e
Ryann <ryann.arruda_159@hotmail.com>
2025-01-17 17:28:24 -0300

    Adding maximum length to username and password fields

---
+++ z_score/src/util/Constraints.java
@@ -0,0 +1,27 @@
+package util;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Constraints {
+	
+	private Constraints() {
+		
+	}
+	
+	public static void setTextFieldMaxLength(TextField tf, int max) {
+		tf.textProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue.length() > max) {
+				tf.setText(oldValue);
+			}
+		});
+	}
+	
+	public static void setPasswordFielddMaxLength(PasswordField pf, int max) {
+		pf.textProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue.length() > max) {
+				pf.setText(oldValue);
+			}
+		});
+	}
+}
