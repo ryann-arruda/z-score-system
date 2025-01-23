daf21536730920bebbf9e8bd81c3b3a6652aba4c
Ryann <ryann.arruda_159@hotmail.com>
2025-01-23 17:14:47 -0300

    Implementing a method to add a new school and helper methods

--- z_score/src/gui/controller/MainController.java
+++ z_score/src/gui/controller/MainController.java
@@ -4,33 +4,31 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import application.Main;
 import entities.Nutritionist;
 import entities.School;
 import entities.service.NutritionistService;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import util.Alerts;
+import util.Utils;
 
 public class MainController implements Initializable{
 	private Nutritionist nutritionist;
-	
-	private NutritionistService service;
-	
-	private Stage currentStage;
 	
 	@FXML
 	private TableView<School> tableViewSchool;
@@ -71,26 +69,13 @@ public class MainController implements Initializable{
 		this.nutritionistIdentifier.setText(nutritionist.getRegionalCouncilNutritionists());
 	}
 	
-	public void setNutritionistService(NutritionistService service) {
-		this.service = service;
-	}
-	
-	public void setCurrentStage(Stage stage) {
-		this.currentStage = stage;
-	}
-	
 	@FXML
-	public void onLogout() {
+	public void onLogout(ActionEvent event) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../../gui/Authentication_view.fxml"));
 			AnchorPane achorPane = loader.load();
 			
-			if(this.currentStage == null) {
-				throw new IllegalStateException("Stage was null");
-			}
-			
-			AuthenticationController authenticationController = loader.getController();
-			authenticationController.setCurrentStage(currentStage);
+			Stage currentStage = Utils.getCurrentStage(event);
 			
 			currentStage.setScene(new Scene(achorPane));
 			
@@ -102,8 +87,31 @@ public class MainController implements Initializable{
 	}
 	
 	@FXML
-	public void onAddNewSchool() {
-
+	public void onAddNewSchool(ActionEvent event) {
+		createDialogForm("../../gui/SchoolForm_view.fxml", Utils.getCurrentStage(event));
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			AnchorPane anchorPane = loader.load();
+			
+			SchoolFormController schoolFormController = loader.getController();
+			schoolFormController.setNutritionist(new Nutritionist());
+			schoolFormController.setNutritionistService(new NutritionistService());
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Cadastro de Escola");
+			dialogStage.setScene(new Scene(anchorPane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.show();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void updateTableViewSchool() {
