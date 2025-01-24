0bfcb79617019503de026c55abbe15721c9a6457
Ryann <ryann.arruda_159@hotmail.com>
2025-01-24 17:46:00 -0300

    Creation of the onSave and onCancel methods. In addition, auxiliary methods were also created.

--- z_score/src/gui/controller/SchoolFormController.java
+++ z_score/src/gui/controller/SchoolFormController.java
@@ -1,10 +1,23 @@
 package gui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import entities.Nutritionist;
+import entities.School;
 import entities.service.NutritionistService;
+import exceptions.FieldValidationException;
+import gui.listeners.DataChangeListener;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import util.Alerts;
+import util.Utils;
 
 public class SchoolFormController {
 	
@@ -12,11 +25,19 @@ public class SchoolFormController {
 	
 	private NutritionistService service;
 	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 	@FXML
 	private TextField name;
 	
 	@FXML
 	private TextField nationalRegistryLegalEntities;
+	
+	@FXML
+	private Label nameError;
+	
+	@FXML
+	private Label nationalRegistryLegalEntitiesError;
 	
 	@FXML
 	private Button save;
@@ -30,5 +51,70 @@ public class SchoolFormController {
 	
 	public void setNutritionistService(NutritionistService service) {
 		this.service = service;
+	}
+	
+	private void validateFields() {
+		FieldValidationException exception = new FieldValidationException("Erros when filling in fields");
+		
+		if(name.getText() == null || name.getText().trim().equals("")) {
+			exception.addError("nameError", "Insira um nome válido!");
+		}
+		
+		if(nationalRegistryLegalEntities.getText() == null || nationalRegistryLegalEntities.getText().trim().equals("")) {
+			exception.addError("nationalRegistryLegalEntitiesError", "Insira um CNPJ válido!");
+		}
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+	}
+	
+	@FXML
+	public void onSave(ActionEvent event) {
+		if(nutritionist == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			validateFields();
+			
+			School school = new School(name.getText(), nationalRegistryLegalEntities.getText());
+			nutritionist.addSchool(school);
+			
+			if(service.update(nutritionist)) {
+				Alerts.showAlert("Sucesso", null, "Cadastro de escola realizado com sucesso!", AlertType.CONFIRMATION);
+				Utils.getCurrentStage(event).close();
+			}
+		}
+		catch(FieldValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+	}
+	
+	@FXML
+	public void onCancel(ActionEvent event) {
+		Utils.getCurrentStage(event).close();
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("nameError")) {
+			nameError.setText(errors.get("nameError"));
+		}
+		else {
+			nameError.setText("");
+		}
+		
+		if(fields.contains("nationalRegistryLegalEntitiesError")) {
+			nationalRegistryLegalEntitiesError.setText(errors.get("nationalRegistryLegalEntitiesError"));
+		}
+		else {
+			nationalRegistryLegalEntitiesError.setText("");
+		}
 	}
 }
