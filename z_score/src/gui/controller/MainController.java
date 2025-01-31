7906e46ee3c9afec1388c7de6e2663a40a381ab9
Ryann <ryann.arruda_159@hotmail.com>
2025-01-31 16:32:43 -0300

    Removing unnecessary import

--- z_score/src/gui/controller/MainController.java
+++ z_score/src/gui/controller/MainController.java
@@ -16,7 +16,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
