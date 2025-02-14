a7ad8a7c444e18d09967a605570074fa7dbf23b6
Ryann <ryann.arruda_159@hotmail.com>
2025-02-14 11:42:13 -0300

    Adding a method to insert dependencies. Implementing the onGoBack method to return to the previous screen and a method to initialize the table's 'SEE' buttons

--- z_score/src/gui/controller/LevelEducationController.java
+++ z_score/src/gui/controller/LevelEducationController.java
@@ -1,24 +1,42 @@
 package gui.controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
+import java.util.ResourceBundle;
 
 import entities.Child;
+import entities.LevelEducation;
+import entities.Nutritionist;
+import entities.School;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import util.Alerts;
 import util.Utils;
 
-public class ChildrenController {
+public class LevelEducationController implements Initializable{
+	
+	private Nutritionist nutritionist;
+	
+	private School school;
+	
+	private LevelEducation levelEducation;
+	
 	@FXML
 	private Label nutritionistName;
 	
@@ -67,6 +85,24 @@ public class ChildrenController {
 	@FXML
 	private Button addNewChild;
 	
+	public void setNutritionist(Nutritionist nutritionist) {
+		this.nutritionist = nutritionist;
+		nutritionistName.setText(nutritionist.getName());
+		nutritionistIdentifier.setText(nutritionist.getRegionalCouncilNutritionists());
+	}
+	
+	public void setSchool(School school) {
+		this.school = school;
+		schoolName.setText(school.getName());
+		schoolIdentifier.setText(school.getNationalRegistryLegalEntities());
+	}
+	
+	public void setLevelEducation(LevelEducation levelEducation) {
+		this.levelEducation = levelEducation;
+		levelEducationName.setText(levelEducation.getName());
+		numberStudentsLevelEducation.setText(String.valueOf(levelEducation.getAllChildren().size()));
+	}
+	
 	@FXML
 	public void onLogout(ActionEvent event) {
 		try {
@@ -82,5 +118,55 @@ public class ChildrenController {
 		} catch (IOException e) {
 			Alerts.showAlert("Erro", null, "Não foi possível fazer log out.", AlertType.ERROR);
 		}
+	}
+	
+	@FXML
+	public void onGoBack(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../../gui/School_view.fxml"));
+			AnchorPane achorPane = loader.load();
+			
+			SchoolController schoolController = loader.getController();
+			schoolController.setNutritionist(nutritionist);
+			schoolController.setSchool(school);
+			schoolController.updateTableViewLevelEducation();
+			
+			Stage currentStage = Utils.getCurrentStage(event);
+			currentStage.setScene(new Scene(achorPane));
+			
+			currentStage.setTitle("Visualização da Escola");
+			currentStage.setResizable(false);
+		} catch (IOException e) {
+			Alerts.showAlert("Erro", null, "Não foi possível carregar a visualização da escola. Tente novamente mais tarde.", AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("date_birth"));
+		tableColumnLastZscoreMeasurement.setCellValueFactory(param -> new SimpleDoubleProperty(param.getValue().getLatestZscoreMeasurement()).asObject());
+	}
+	
+	private void initSeeButtons() {
+		tableColumnSEE.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue()));
+		tableColumnSEE.setCellFactory(cell -> new TableCell<Child, Child>(){
+			private final Button button = new Button("Ver");
+			private final StackPane stackPane = new StackPane(button);
+			
+			@Override
+			protected void updateItem(Child child, boolean empty) {
+				super.updateItem(child, empty);
+				
+				if(child == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				button.prefWidth(65.0);
+				button.setOnAction(null);
+				setGraphic(stackPane);
+			}
+		});
 	}
 }
