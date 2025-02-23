b345bea0c7ee2c82507b8c7db7f2fb4aad1bde71
Ryann <ryann.arruda_159@hotmail.com>
2025-02-23 10:57:56 -0300

    Implementing the onSave method and helper methods

--- z_score/src/gui/controller/ChildFormController.java
+++ z_score/src/gui/controller/ChildFormController.java
@@ -1,22 +1,35 @@
 package gui.controller;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import db.DBException;
+import entities.Child;
+import entities.LevelEducation;
 import entities.Nutritionist;
 import entities.service.NutritionistService;
+import exceptions.FieldValidationException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import util.Alerts;
 import util.Utils;
 
 public class ChildFormController implements Initializable{
 	
 	private Nutritionist nutritionist;
+	
+	private LevelEducation levelEducation;
 	
 	private NutritionistService service;
 	
@@ -46,6 +59,65 @@ public class ChildFormController implements Initializable{
 		this.service = service;
 	}
 	
+	public void setLevelEducation(LevelEducation levelEducation) {
+		this.levelEducation = levelEducation;
+	}
+	
+	private void validateFields() {
+		FieldValidationException exception = new FieldValidationException("Erros when filling in fields");
+		
+		if(childName.getText() == null || childName.getText().trim().equals("")) {
+			exception.addError("childNameError", "Insira um nome válido!");
+		}
+		
+		if(childDateBirth.getValue() == null) {
+			exception.addError("childDateBirthError", "Insira uma data de nascimento válida!");
+		}
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+	}
+	
+	private Date getDateBirth() {
+		LocalDate localDate = childDateBirth.getValue();
+		
+		return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	}
+	
+	@FXML
+	public void onSave(ActionEvent event) {
+		if(nutritionist == null) {
+			throw new IllegalStateException("Nutritionist entity was null");
+		}
+		
+		if(levelEducation == null) {
+			throw new IllegalStateException("LevelEducation entity was null");
+		}
+		
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			validateFields();
+			
+			Child child = new Child(childName.getText(), getDateBirth());
+			levelEducation.addChild(child);
+			
+			if(service.update(nutritionist)) {
+				Alerts.showAlert("Sucesso", null, "Cadastro de aluno realizado com sucesso!", AlertType.CONFIRMATION);
+				Utils.getCurrentStage(event).close();
+			}
+		}
+		catch(FieldValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DBException e) {
+			Alerts.showAlert("Erro", null, "Não foi possível cadastrar um novo aluno. Tente novamente mais tarde.", AlertType.ERROR);
+		}
+	}
+	
 	@FXML
 	public void onCancel(ActionEvent event) {
 		Utils.getCurrentStage(event).close();
@@ -54,5 +126,23 @@ public class ChildFormController implements Initializable{
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		childDateBirth.getEditor().setDisable(true);
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("childNameError")) {
+			childNameError.setText(errors.get("childNameError"));
+		}
+		else {
+			childNameError.setText("");
+		}
+		
+		if(fields.contains("childDateBirthError")) {
+			childDateBirthError.setText(errors.get("childDateBirthError"));
+		}
+		else {
+			childDateBirthError.setText("");
+		}
 	}
 }
