d8973c91cb8fc2a0932970f5a91e02e18f9ba4ca
Ryann <ryann.arruda_159@hotmail.com>
2025-03-03 17:04:37 -0300

    Initial implementation of a controller for MeasurementForm_view

---
+++ z_score/src/gui/controller/MeasurementFormController.java
@@ -0,0 +1,57 @@
+package gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import entities.Child;
+import entities.Nutritionist;
+import entities.service.NutritionistService;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MeasurementFormController implements Initializable{
+	private Nutritionist nutritionist;
+	
+	private NutritionistService service;
+	
+	private Child child;
+	
+	@FXML
+	private DatePicker date;
+	
+	@FXML
+	private Label dateError;
+	
+	@FXML
+	private TextField value;
+	
+	@FXML
+	private Label valueError;
+	
+	@FXML
+	private Button save;
+	
+	@FXML
+	private Button cancel;
+	
+	public void setNutritionist(Nutritionist nutritionist) {
+		this.nutritionist = nutritionist;
+	}
+	
+	public void setNutritionistService(NutritionistService service) {
+		this.service = service;
+	}
+	
+	public void setChild(Child child) {
+		this.child = child;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		date.getEditor().setDisable(true);
+	}
+}
