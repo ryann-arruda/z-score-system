d5de7444d752e3a91327fd8ad35bcee4847a825a
Ryann <ryann.arruda_159@hotmail.com>
2025-03-03 17:08:14 -0300

    Adding a method to create a dialog form to add a measure

--- z_score/src/gui/controller/ChildController.java
+++ z_score/src/gui/controller/ChildController.java
@@ -1,5 +1,6 @@
 package gui.controller;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
@@ -7,11 +8,20 @@ import entities.Child;
 import entities.MeasurementZscore;
 import entities.Nutritionist;
 import entities.service.NutritionistService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import util.Alerts;
+import util.Utils;
 
 public class ChildController {
 	
@@ -63,5 +73,47 @@ public class ChildController {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		birthDate.setText(sdf.format(child.getDateBirth()));
+	}
+	
+	@FXML
+	public void onGoBack(ActionEvent event) {
+		Utils.getCurrentStage(event).close();
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			AnchorPane anchorPane = loader.load();
+			
+			if(nutritionist == null) {
+				throw new IllegalStateException("Nutritionist entity was null");
+			}
+			
+			if(child == null) {
+				throw new IllegalStateException("Child entity was null");
+			}
+			
+			MeasurementFormController measurementFormController = loader.getController();
+			measurementFormController.setNutritionist(nutritionist);
+			measurementFormController.setChild(child);
+			measurementFormController.setNutritionistService(new NutritionistService());
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Cadastro de Nova Medida Z-score");
+			dialogStage.setScene(new Scene(anchorPane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.show();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("Erro", null, "Não foi possível abrir a tela de cadastro de nova medida Z-score. Tente novamente mais tarde.", AlertType.ERROR);
+		}
+	}
+	
+	@FXML
+	public void onAddNewMeasurement(ActionEvent event) {
+		createDialogForm("../../gui/MeasurementForm_view.fxml", Utils.getCurrentStage(event));
 	}
 }
