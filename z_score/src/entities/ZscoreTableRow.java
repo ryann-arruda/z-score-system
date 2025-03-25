d3f96872fc88350945d5eeb61647627975481dac
Ryann <ryann.arruda_159@hotmail.com>
2025-03-25 16:29:40 -0300

    Removing month attribute

--- z_score/src/entities/ZscoreTableRow.java
+++ z_score/src/entities/ZscoreTableRow.java
@@ -4,13 +4,11 @@ import java.util.Objects;
 
 public class ZscoreTableRow {
 	private Long id;
-	private Integer month;
 	private Double median;
 	private Double stdDeviation;
 	
-	public ZscoreTableRow(Long id, Integer month, Double median, Double stdDeviation) {
+	public ZscoreTableRow(Long id, Double median, Double stdDeviation) {
 		this.id = id;
-		this.month = month;
 		this.median = median;
 		this.stdDeviation = stdDeviation;
 	}
@@ -21,14 +19,6 @@ public class ZscoreTableRow {
 	
 	public void setId(Long id) {
 		this.id = id;
-	}
-	
-	public Integer getMonth() {
-		return month;
-	}
-	
-	public void setMonth(Integer month) {
-		this.month = month;
 	}
 	
 	public Double getMedian() {
@@ -46,10 +36,10 @@ public class ZscoreTableRow {
 	public void setStdDeviation(Double stdDeviation) {
 		this.stdDeviation = stdDeviation;
 	}
-
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, median, month, stdDeviation);
+		return Objects.hash(id, median, stdDeviation);
 	}
 
 	@Override
@@ -62,14 +52,13 @@ public class ZscoreTableRow {
 			return false;
 		ZscoreTableRow other = (ZscoreTableRow) obj;
 		return Objects.equals(id, other.id) && Objects.equals(median, other.median)
-				&& Objects.equals(month, other.month) && Objects.equals(stdDeviation, other.stdDeviation);
+				&& Objects.equals(stdDeviation, other.stdDeviation);
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("ID (" + id.toString() + ") - ");
-		sb.append("Mês (" + month.toString() + ") - ");
 		sb.append("Mediana (" + median.toString() +  ") - ");
 		sb.append("Desvio Padrão (" + stdDeviation.toString() + ")");
 		
