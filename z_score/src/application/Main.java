77a0c6978217ef7314ff8be7fb77e966d2806229
Ryann <ryann.arruda_159@hotmail.com>
2025-05-14 12:31:49 -0300

    Adding the icon to the screens

--- z_score/src/application/Main.java
+++ z_score/src/application/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -18,11 +19,18 @@ public class Main extends Application{
 			stage.setTitle("NutriData");
 			stage.setScene(scene);
 			stage.setResizable(false);
+			addIcon(stage);
 			stage.show();
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void addIcon(Stage stage) {
+		Image icon = new Image(getClass().getResource("../resources/logo.png").toString());
+		
+		stage.getIcons().add(icon);
 	}
 	
 	public static void main(String[] args) {
